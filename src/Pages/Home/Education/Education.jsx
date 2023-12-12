ddf2a1bc3edc29761233ca543f4cd3441e6863e4
rhmunna143 <rhmunna19@gmail.com>
2023-12-12 18:41:22 +0600

    almost done

--- src/Pages/Home/Education/Education.jsx
+++ src/Pages/Home/Education/Education.jsx
@@ -13,22 +13,22 @@ const Education = () => {
                     <li>
                         <div className="timeline-start">2017</div>
                         <div className="timeline-middle">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 text-blue-600"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
                         </div>
                         <div className="timeline-end timeline-box">SSC - Science <br />
                          Jessore Board (GPA: 4.55)</div>
-                        <hr />
+                        <hr className='bg-primary' />
                     </li>
 
                     <li>
-                        <hr />
+                        <hr className='bg-primary' />
                         <div className="timeline-start">2019</div>
                         <div className="timeline-middle">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 text-blue-600"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
                         </div>
                         <div className="timeline-end timeline-box">HSC - Science <br /> 
                         Jessore Board (GPA: 4.42)</div>
-                        <hr />
+                        <hr className='bg-primary' />
                     </li>
 
                     <li>
