22fdf7a28c6be25ed18cce5559a61e94b176203e
rhmunna143 <rhmunna19@gmail.com>
2024-01-28 21:41:13 +0600

    name and resume btn fixed

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -11,7 +11,7 @@ const Banner = () => {
         <div className="banner-bg  lg:py-16 flex flex-col justify-center items-start">
             <Container>
                 <div data-aos="fade-left" className="banner-black p-16 lg:w-4/5">
-                    <h1 className="text-5xl font-bold">Hi, I am <span className="uppercase">Rabi Haque <span className="text-primary">Munna</span></span></h1>
+                    <h1 className="text-5xl font-bold">Hi, I am <span className="uppercase">Rabbi Haque <span className="text-primary">Munna</span></span></h1>
 
                     <h2 className="text-4xl mt-6 font-bold capitalize text-primary">Web Developer</h2>
 
@@ -22,7 +22,7 @@ const Banner = () => {
 
                     {/* RESUME Update */}
                     <div className="random-buttons flex flex-col justify-center w-fit gap-6">
-                        <a href={CV} download={"MD_RABBI_HAQUE_MUNNA-Resume.pdf"} className="btn w-fit mt-6 btn-outline btn-warning text-lg font-medium uppercase"><HiOutlineDownload /> Download Resume</a>
+                        <a href={CV} download={"MD_RABBI_HAQUE_MUNNA-Resume.pdf"} className="btn w-fit text-sm mt-6 btn-outline btn-warning md:text-lg font-medium uppercase"><HiOutlineDownload /> Download Resume</a>
                         <div className="icons">
                             <SocialIcons />
                         </div>
